92e754638f06efbf3ae70d8614b217212d9d5d38
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2022-10-10 07:33:06 +0200

    deleted coma from title

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let age = 32
+
+age = 50
+
+const casatorit = false
+
+const name = "Daniel"
+
+const lista = ["marfa", "lapte", 12, age]
+
+const persoana = {
+    name: name,
+    age: age
+}
+
+console.log(persoana)
